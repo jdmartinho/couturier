65335dfece1a98366fb347838d34abc4067fd713
João Martinho <jdmartinho@gmail.com>
2024-05-02 13:09:40 +0100

    Initial commit with project from Spring Initialzr using Java 17 and Gradle with Kotlin. Added also Spring Web dependency.

---
+++ src/main/java/com/goblinrocketlabs/couturier/CouturierApplication.java
@@ -0,0 +1,13 @@
+package com.goblinrocketlabs.couturier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouturierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouturierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goblinrocketlabs/couturier/CouturierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goblinrocketlabs.couturier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouturierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
